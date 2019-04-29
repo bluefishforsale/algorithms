164fb6adf9ce829a3df7f162b675323540837999
tskiens-quizlet <terrac.skien@quizlet.com>
2019-04-29 15:49:49 -0700

    YAY! finally an algorithm that solves the obsticle course!

--- path_finding/pathfinder2.py
+++ path_finding/pathfinder2.py
@@ -10,12 +10,25 @@ import os
 
 counter = 0
 
+class Memoize:
+    """Remember things for me"""
+
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+	    self.memo[args] = self.fn(*args)
+        return self.memo[args]
+
 class Node():
     """A node class for A* Pathfinding"""
 
-    def __init__(self, parent=None, position=None):
+    def __init__(self, parent=None, position=None, dead=None):
         self.parent = parent
         self.position = position
+        self.dead = dead
 
         self.g = 0
         self.h = 0
@@ -24,7 +37,7 @@ class Node():
     def __eq__(self, other):
         return self.position == other.position
 
-def astar(maze, start, end):
+def astar(maze, start, end, rpath={}):
     global counter
     """Returns a list of tuples as a path from the given start to the given end in the given maze"""
 
@@ -55,17 +68,16 @@ def astar(maze, start, end):
         # Pop current off open list, add to closed list
         open_list.pop(current_index)
         closed_list.append(current_node)
-        print(u"\033[{};{}H\u001b[{}m ".format(current_node.position[0], current_node.position[1], 41))
-        sleep(1/720.0)
+        print(u"\033[{};{}H\u001b[{}m \u001b[0m".format(current_node.position[0], current_node.position[1], 41))  # red
+        sleep(1/1440.0)
 
-        # Found the goal
+        # Found the goal and Return path
         if current_node == end_node:
             path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
+            while current_node is not None:
+                path.append(current_node.position)
+                current = current_node.parent
+                return path[::-1] # Return reversed path
 
         # Generate children
         children = []
@@ -74,7 +86,7 @@ def astar(maze, start, end):
             # Get node position
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-            # Make sure within range
+            # Make sure nodes is within grid range
             if node_position[0] > (len(maze) - 1) \
                 or node_position[0] < 0 \
                 or node_position[1] > (len(maze[0]) -1) \
@@ -85,11 +97,16 @@ def astar(maze, start, end):
             if maze[node_position[0]][node_position[1]]:
                 continue
 
-            # Create new node
+            # No failures? ok, create the child
             new_node = Node(current_node, node_position)
 
-            # Append
+            # have we been here before?
+            if new_node in closed_list:
+                continue
+
+            # New node becomes a child!
             children.append(new_node)
+            closed_list.append(current_node)
 
         # Loop through children
         for child in children:
@@ -98,13 +115,17 @@ def astar(maze, start, end):
             # Child is on the closed list
             for closed_child in closed_list:
                 if child == closed_child:
+                    child.dead = True
                     continue
 
             # Create the f, g, and h values
+            # g = distance from start
+            # h = a heuristic, end - start**2
+            # f = the factor we compare in the end
             child.g = current_node.g + 1
             child.h = \
-                ( (end_node.position[0] - child.position[0]) ** 2) + \
-                ( (end_node.position[1] - child.position[1]) ** 2)
+                ( (end_node.position[0] - child.position[0]) **2) + \
+                ( (end_node.position[1] - child.position[1]) **2)
             child.f = child.g + child.h
 
             # Child is already in the open list
@@ -114,8 +135,8 @@ def astar(maze, start, end):
 
             # Add the child to the open list
             open_list.append(child)
-            print(u"\033[{};{}H\u001b[{}m ".format(child.position[0], child.position[1], 43  ))
-            path = [ x.position for x in closed_list ]
+            print(u"\033[{};{}H\u001b[{}m \u001b[0m".format(child.position[0], child.position[1], 43  )) # yellow
+
 
 # GRID Printer
 # Calls route picker for path highlighting
@@ -190,6 +211,7 @@ if __name__ == '__main__':
     # pathfinding
     print_grid(board, [])
     path = astar(board, start, end)
+    #path = astar(board, end, start)
     print(u'\033[{};{}H\u001b[0m'.format(end[1], 0))
     print(u"Start: {} end: {}, bad: {}".format(start, end, bad))
     print(u"Path lenth: {}".format(len(path)))
