0378076b33def439c4fe7ce0ce111f8ed2ccf6ce
tskiens-quizlet <terrac.skiens@quizlet.com>
2021-10-22 11:15:19 -0700

    but in inner loop

--- MaxProductSubarray.py
+++ MaxProductSubarray.py
@@ -1,22 +1,24 @@
 data = [2, 3, -2, 4]
-# data = [2, 3, -2, 4, 2, 3, 1]
+data =  [ -1, -2, 0 ]
 
-def product(input):
+def product(value):
   product = 1
-  for x in input:
+  for x in value:
     product = product * x
   return product
 
-def max_product_subarray(input):
-  subarray = input
+def max_product_subarray(value):
+  subarray = value
   p = product(subarray)
   # walk the full length
-  for i in range(0, len(input) -1):
+  for i in range(0, len(value) -1):
   # walk increasing ranges starting at i
-    for j in range(1, len(input) -1):
-      if product(input[i:i+j]) >= p:
-        subarray = input[i:i+j]
-        p = product(input[i:i+j]) 
+    for j in range(1, len(value)):
+      print(i, j, value[i:i+j], product(value[i:i+j]))
+      if product(value[i:i+j]) >= p:
+        subarray = value[i:i+j]
+        p = product(value[i:i+j]) 
   return p, subarray
 
+print(data)
 print(max_product_subarray(data))
